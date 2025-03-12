87130656df34f2b2d3bcded4a64bcc4bb77ce6a9
n3s-online <n3s.online@gmail.com>
2025-03-11 18:18:08 -0700

    Add padding to How to Play sheet content

--- src/components/GameDisplay.tsx
+++ src/components/GameDisplay.tsx
@@ -215,7 +215,7 @@ export function GameDisplay() {
                 How to Play
               </Button>
             </SheetTrigger>
-            <SheetContent className="overflow-y-auto">
+            <SheetContent className="overflow-y-auto p-2">
               <SheetHeader>
                 <SheetTitle className="text-2xl text-blue-800 dark:text-blue-400">
                   How to Play Ski-O-Guessr
